6b61eaa5f226d08c554cefcb3dacb045b1c3bbda
Antony <akb@akb.me.uk>
2016-03-02 11:40:56 +0000

    Untabify.

--- EPubShrink.ps1
+++ EPubShrink.ps1
@@ -1,7 +1,7 @@
 #*********************************************************************#
-#***	This software is in the public domain, furnished "as is",  ***#
-#***	without technical support, and with no warranty, express   ***#
-#***	or implied, as to its usefulness for any purpose.		   ***#
+#***    This software is in the public domain, furnished "as is",  ***#
+#***    without technical support, and with no warranty, express   ***#
+#***    or implied, as to its usefulness for any purpose.          ***#
 #*********************************************************************#
 
 param(
